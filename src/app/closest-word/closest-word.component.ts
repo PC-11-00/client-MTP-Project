57ea7b3010b8e04a22ffbc5b8542221de7cc0330
Pushpendra <76156941+PC-11-00@users.noreply.github.com>
2024-05-27 13:04:33 +0530

    changed some files

--- src/app/closest-word/closest-word.component.ts
+++ src/app/closest-word/closest-word.component.ts
@@ -1,23 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
+import { debounceTime, distinctUntilChanged, switchMap, catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
 
 @Component({
   selector: 'app-closest-word',
   templateUrl: './closest-word.component.html',
   styleUrls: ['./closest-word.component.scss']
 })
-export class ClosestWordComponent {
-  inputWord: string = '';
-  closestWord: string = '';
+export class ClosestWordComponent implements OnInit {
+  inputWordControl = new FormControl('');
+  suggestion: any;
 
   constructor(private http: HttpClient) { }
 
-  async getClosestWord() {
-    try {
-      const response: any = await this.http.post('http://127.0.0.1:5000/get_closest_word', { input_word: this.inputWord }).toPromise();
-      this.closestWord = response.closest_word || 'No closest word found';
-    } catch (error) {
-      console.error('Error fetching closest word:', error);
+  ngOnInit(): void {
+    this.inputWordControl.valueChanges
+      .pipe(
+        debounceTime(300), // Wait for 300ms after each keystroke
+        distinctUntilChanged(), // Only send request if the value has changed
+        switchMap((inputWord: string) => this.fetchSuggestions(inputWord)),
+        catchError(error => {
+          console.error('Error fetching suggestions:', error);
+          return throwError('Failed to fetch suggestions. Please try again.');
+        })
+      )
+      .subscribe((suggestions: string[]) => {
+        console.log(suggestions);
+        this.suggestion = suggestions;
+        console.log(this.suggestion)
+      });
+  }
+
+  fetchSuggestions(inputWord: string) {
+    if (inputWord.trim() !== '') { // Check if inputWord is not empty after trimming whitespace
+      return this.http.post<string[]>('http://127.0.0.1:5000/get_suggestions', { input_word: inputWord })
+        .pipe(
+          catchError(error => {
+            console.error('Error fetching suggestions:', error);
+            return throwError('Failed to fetch suggestions. Please try again.');
+          })
+        );
+    } else {
+      // Return an observable with an empty array if inputWord is empty
+      return [];
     }
+  }
+
+
+  isArray(value: any): boolean {
+    return Array.isArray(value);
   }
 }
